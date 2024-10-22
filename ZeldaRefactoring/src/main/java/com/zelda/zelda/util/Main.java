8a3b1f43a28b8f5b44c84cf199b6c211a8efc223
Tony-UY <164003649+Tony-UY@users.noreply.github.com>
2024-10-22 14:59:33 +0200

    Delete ZeldaRefactoring directory

--- ZeldaRefactoring/src/main/java/com/zelda/zelda/util/Main.java
+++
@@ -1,8 +0,0 @@
-package com.zelda.zelda.util;
-
-public class Main {
-    public static void main(String[] args) {
-        // Remplacez par le chemin absolu ou relatif approprié du fichier JSON
-        String jsonFilePath = "src/main/java/com/zelda/zelda/util/MapZelda.json";
-    }
-}
