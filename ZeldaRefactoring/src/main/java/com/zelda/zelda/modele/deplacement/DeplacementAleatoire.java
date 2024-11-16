4035dac0d81b7ab2f6398468b0862d5af9830cb1
Tony Uy <tuy@iut.univ-paris8.fr>
2024-11-16 21:17:57 +0100

    A MERGE DEMAIN LES GARS j'ai modif bcp

---
+++ ZeldaRefactoring/src/main/java/com/zelda/zelda/modele/deplacement/DeplacementAleatoire.java
@@ -0,0 +1,8 @@
+package com.zelda.zelda.modele.deplacement;
+
+public class DeplacementAleatoire implements StrategieDeplacement{
+
+    public void seDeplace(){
+
+    }
+}
---
+++ ZeldaRefactoring/src/main/java/com/zelda/zelda/modele/deplacement/StrategieDeplacement.java
@@ -0,0 +1,9 @@
+package com.zelda.zelda.modele.deplacement;
+
+import com.zelda.zelda.modele.acteur.Monstre;
+
+public interface StrategieDeplacement {
+
+    public void seDeplace();
+
+}
